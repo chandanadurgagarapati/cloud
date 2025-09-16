77ec31956f045649a74f7e4945ea2aebaa3db0de
chandanadurgagarapati <chandu366327@gmail.com>
2025-09-16 15:08:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+region = "us-east-1"
 }
 
-resource "aws_instance" "demo" {
-  ami           = "ami-0b09ffb6d8b58ca91" # Amazon Linux 2 (us-east-1)
-  instance_type = "t3.micro"
+resource "aws_instance" "one" {
+ami = "ami-0b09ffb6d8b58ca91"
+instance_type = "t3.micro"
+tags = {
+Name = "hcp-server"
+}
 }
